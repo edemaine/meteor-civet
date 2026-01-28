3f84a5e235738e47d220082fb5fbe76a11226136
Erik Demaine <edemaine@mit.edu>
2026-01-27 19:53:45 -0500

    Test config

--- tests/civet_tests.js
+++ tests/civet_tests.js
@@ -1,6 +1,7 @@
-import { civetAdd, civetAnswer } from './civet_test.civet'
+import { civetAdd, civetAnswer, civetBool } from './civet_test.civet'
 
 Tinytest.add('edemaine:civet - compiles civet files', (test) => {
   test.equal(civetAnswer, 42)
+  test.equal(civetBool, true)
   test.equal(civetAdd(1, 2), 3)
 })
